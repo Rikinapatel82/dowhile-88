e65fa140e9b94dde0c736db37db3869c99e69475
Rikinapatel82 <149239614+Rikinapatel82@users.noreply.github.com>
2023-11-20 19:31:54 +0000

    Add files via upload

---
+++ DoWhileTable8.java
@@ -0,0 +1,13 @@
+package dowhiletable8;
+
+public class DoWhileTable8 {
+    public static void main(String[] args) {
+        int i=1;
+        do
+        {
+                System.out.println("8 *" +i+ "=" +i*8); i++;
+        }
+            while(i <=20);
+        }
+    }
+
